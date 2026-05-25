f0d298121584e2d5208182e4d776ea0f194dc727
Julian Quintero <julianquinteromartinez35@gmail.com>
2026-05-25 00:40:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,10 @@ app.config['SESSION_REFRESH_EACH_REQUEST'] = True
 # ==============================================
 database_url = os.environ.get('DATABASE_URL', '')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///test.db"
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     'pool_size': 10,
